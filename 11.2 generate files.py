dffa8c95493c9d9bf4fde6fd0ee5b691fc666c61
Jayson Galvez <sendnoose@gmail.com>
2022-10-25 19:42:29 +0800

    update regex
    need to fix swap function

--- 11.2 generate files.py
+++ 11.2 generate files.py
@@ -2,11 +2,11 @@
 
 import os
 
-spam = ['ppppp 6/12/2009 ppppp', 'ppppp 6/13/2009 ppppp', 'ppppp 3/13/2010 ppppp', 'ppppp 1/14/2011 ppppp', 'ppppp 5/22/2012 ppppp', 'ppppp 3/24/2013 ppppp', 'ppppp 7/30/2014 ppppp', 'ppppp 8/16/2015 ppppp', 'ppppp 9/12/2015 ppppp', 'ppppp 6/22/2016 ppppp', 'ppppp 9/4/2016 ppppp', 'ppppp 10/19/2016 ppppp', 'ppppp 12/30/2016 ppppp', 'ppppp 1/5/2017 ppppp', 'ppppp 6/18/2018 ppppp', 'ppppp 10/13/2018 ppppp', 'ppppp 2/3/2020 ppppp', 'ppppp 8/17/2020 ppppp', 'ppppp 12/13/2021 ppppp', 'ppppp 6/8/2022 ppppp']
+spam = ['ppppp 6-12-2009 ppppp', 'ppppp 6-13-2009 ppppp', 'ppppp 3-13-2010 ppppp', 'ppppp 1-14-2011 ppppp', 'ppppp 5-22-2012 ppppp', 'ppppp 3-24-2013 ppppp', 'ppppp 7-30-2014 ppppp', 'ppppp 8-16-2015 ppppp', 'ppppp 9-12-2015 ppppp', 'ppppp 6-22-2016 ppppp', 'ppppp 9-4-2016 ppppp', 'ppppp 10-19-2016 ppppp', 'ppppp 12-30-2016 ppppp', 'ppppp 1-5-2017 ppppp', 'ppppp 6-18-2018 ppppp', 'ppppp 10-13-2018 ppppp', 'ppppp 2-3-2020 ppppp', 'ppppp 8-17-2020 ppppp', 'ppppp 12-13-2021 ppppp', 'ppppp 6-8-2022 ppppp']
 
 os.chdir(r'''c:\\Users\\sulla\\Downloads\\Automate the Boring Stuff with Python Programming\\[TutsNode.com] - Automate the Boring Stuff with Python Programming\\11. Files''')
 if not os.path.isdir('.\\generated'):
     os.mkdir(r'''.\\generated''')
 for filename in spam:
     print(filename)
-    fileObj = open('''.\\generated\\''' + 'ppppp 6/12/2009 ppppp', 'w')
+    fileObj = open('''.\\generated\\''' + filename, 'w')
--- 11.2 rename date american to european.py
+++ 11.2 rename date american to european.py
@@ -1,11 +1,71 @@
-import shutil, re
+import shutil, re, os
 
 # TODO: regex for american dates
 usdateRegex = re.compile(r'''
+(?P<DATE>
+  (?P<MM>
+    # check first digit if 0 or 1
+    (?P<M>[01])?
+    # ternary operator, If previous is 0 or 1 ? True : False
+    (?(M)
+        # if True, second digit can only be 0,1,2
+        [012]
+        # if False, second digit can't be 2 or 3
+        |(?<![123])\d
+    )
+  )
+  
+  [-]
+  
+  (?P<DD>
+    [0123]?\d
+  )
+  
+  [-]
+  
+  (?P<YYYY>
+    [12]\d\d\d
+  )
+)
 ''', re.VERBOSE)
+'''
+[0] DATE
+[1] MM
+[2] M
+[3] DD
+[4] YYYY
+'''
 
-# TODO: ask which directory to work in
+# ask which directory to work in
 def askDirectory():
     workDir = input()
+    while not os.path.exists(workDir):
+        workDir = input()
+    return workDir
+
+# DEBUG: remove function after testing
+def DEBUG_askDirectory():
+    workDir = r'''C:\Users\sulla\Downloads\Automate the Boring Stuff with Python Programming\[TutsNode.com] - Automate the Boring Stuff with Python Programming\11. Files\generated'''
+    while not os.path.exists(workDir):
+        workDir = input()
+    return workDir
 
 # TODO: scan whole directory
+def scanTree(rootDir):
+    for root, dirs, files in os.walk(rootDir):
+        for name in files:
+            date = re.findall(usdateRegex, name)
+            newname = name
+            # change US to EUR date
+            if len(date) > 0:
+                print('found us date:  ' + name)
+                newname = swaptext(name, date[0][1], date[0][3])
+                print('us to eur date: ' + newname)
+                print()
+                
+def swaptext(text, a, b):
+    return b.join(part.replace(b, a) for part in text.split(a))
+
+
+workDir = DEBUG_askDirectory()
+scanTree(workDir)
