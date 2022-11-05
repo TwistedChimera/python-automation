a3f7da4c7dc2e601d668499471e2d45872f58c6e
Jayson Galvez <sendnoose@gmail.com>
2022-11-05 14:58:59 +0800

    fixed class name

--- scripts/selenium_example_1.py
+++ scripts/selenium_example_1.py
@@ -1,11 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
 from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 
 browser = webdriver.Firefox(service=Service(GeckoDriverManager(path = r".\\Drivers").install()))
 browser.get('https://inventwithpython.com')
 try:
-    elem = browser.find_element_by_class_name(' cover-thumb')
+    elem = browser.find_element(By.CLASS_NAME, 'card-img-top')
     print('Found <%s> element with that class name!' % (elem.tag_name))
 except:
     print('Was not able to find an element with that name.')
